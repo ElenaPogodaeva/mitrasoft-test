7a0df3cf349b609889268732965d7d4f852704f3
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2023-06-05 17:55:36 +0300

    feat: implement comment display

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,16 +1,20 @@
 import { ListGroup } from 'react-bootstrap';
 import { Comment } from '../Comment/Comment';
-import { IComment } from '../../types/types';
+import { useAppSelector } from '../../hooks/hooks';
 
 type CommentsProps = {
-  comments: IComment[];
+  postId: number;
 };
 
-export const Comments = ({ comments }: CommentsProps) => {
+export const Comments = ({ postId }: CommentsProps) => {
+  const { comments } = useAppSelector((state) => state.comment);
+
+  const filteredComments = comments.filter((comment) => comment.postId === postId);
+
   return (
     <ListGroup className="list-group-flush">
       {Boolean(comments.length) &&
-        comments.map((comment) => <Comment key={comment.id} comment={comment} />)}
+        filteredComments.map((comment) => <Comment key={comment.id} comment={comment} />)}
     </ListGroup>
   );
 };
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,60 +1,8 @@
-import { Button, Card, Col } from 'react-bootstrap';
+import { Button, Card, Col, Image } from 'react-bootstrap';
 import { IPost } from '../../types/types';
 import Comments from '../Comments/Comments';
 import './Post.scss';
-
-const comments = [
-  {
-    postId: 1,
-    id: 1,
-    name: 'id labore ex et quam laborum',
-    email: 'Eliseo@gardner.biz',
-    body: `laudantium enim quasi est quidem magnam voluptate ipsam eos
-tempora quo necessitatibus
-dolor quam autem quasi
-reiciendis et nam sapiente accusantium`,
-  },
-  {
-    postId: 1,
-    id: 2,
-    name: 'quo vero reiciendis velit similique earum',
-    email: 'Jayne_Kuhic@sydney.com',
-    body: `est natus enim nihil est dolore omnis voluptatem numquam
-et omnis occaecati quod ullam at
-voluptatem error expedita pariatur
-nihil sint nostrum voluptatem reiciendis et`,
-  },
-  {
-    postId: 1,
-    id: 3,
-    name: 'quo vero reiciendis velit similique earum',
-    email: 'Jayne_Kuhic@sydney.com',
-    body: `est natus enim nihil est dolore omnis voluptatem numquam
-et omnis occaecati quod ullam at
-voluptatem error expedita pariatur
-nihil sint nostrum voluptatem reiciendis et`,
-  },
-  {
-    postId: 1,
-    id: 4,
-    name: 'quo vero reiciendis velit similique earum',
-    email: 'Jayne_Kuhic@sydney.com',
-    body: `est natus enim nihil est dolore omnis voluptatem numquam
-et omnis occaecati quod ullam at
-voluptatem error expedita pariatur
-nihil sint nostrum voluptatem reiciendis et`,
-  },
-  {
-    postId: 1,
-    id: 5,
-    name: 'quo vero reiciendis velit similique earum',
-    email: 'Jayne_Kuhic@sydney.com',
-    body: `est natus enim nihil est dolore omnis voluptatem numquam
-et omnis occaecati quod ullam at
-voluptatem error expedita pariatur
-nihil sint nostrum voluptatem reiciendis et`,
-  },
-];
+import { useState } from 'react';
 
 type PostProps = {
   post: IPost;
@@ -62,7 +10,12 @@ type PostProps = {
 };
 
 export const Post = ({ post, handleClick }: PostProps) => {
-  const { id, title, body } = post;
+  const { id, title, body, userId } = post;
+  const [isCommentOpen, setIsCommentOpen] = useState(false);
+
+  const handleBtnClick = () => {
+    setIsCommentOpen((prev) => !prev);
+  };
 
   return (
     <Col>
@@ -70,13 +23,12 @@ export const Post = ({ post, handleClick }: PostProps) => {
         <Card.Body className="card">
           <Card.Title className="card-title">{title}</Card.Title>
           <Card.Text className="card-text">{body}</Card.Text>
-          {/* <Card.Img src={logo} style={{ width: 100 }} /> */}
-          {/* <Button variant="primary">Comments</Button> */}
-          {/* <Comments comments={comments} /> */}
         </Card.Body>
         <Card.Footer className="text-muted">
-          <Button variant="primary">Комментарии</Button>
-          <Comments comments={comments} />
+          <Button variant="primary" onClick={handleBtnClick}>
+            Комментарии
+          </Button>
+          {isCommentOpen && <Comments postId={id} />}
         </Card.Footer>
       </Card>
     </Col>
