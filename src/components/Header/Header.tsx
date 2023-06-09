327d9229003edeccc341384cae2d8e3cf437bbe3
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2023-06-09 15:08:00 +0300

    feat: add Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,34 @@
-import { Container } from 'react-bootstrap';
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import './Header.scss';
 
 export const Header = () => {
-  return <Container>header</Container>;
+  return (
+    <Navbar expand="lg" variant="light" bg="light">
+      <Container>
+        <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar-expand-sm"
+          aria-labelledby="offcanvasNavbarLabel-expand-sm"
+          placement="start"
+        >
+          <Offcanvas.Header closeButton className="align-items-start">
+            <Offcanvas.Title id="offcanvasNavbarLabel-expand-sm">
+              Погодаева Елена
+              <a className="email" href="mailto:pogodaeva.elena2020@gmail.com">
+                pogodaeva.elena2020@gmail.com
+              </a>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-start flex-grow-1 pe-3">
+              <Nav.Link href="/">Список постов</Nav.Link>
+              <Nav.Link href="/about">Обо мне</Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
